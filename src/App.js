009aa469ed949208d2519ba3b971a39e735baf1f
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-25 16:37:50 -0500

    Made function and button to change text content of h1 tag for practicing tests with event listeners

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import './App.css';
+import React, { useState } from 'react';
 
-const App = () => <h1>Our First Test</h1>;
+const App = () => {
+  const [heading, setHeading] = useState('Magnificent Monkeys');
+
+  const clickHandler = () => {
+    setHeading('Radical Rhinos');
+  };
+
+  return (
+    <>
+      <button type='button' onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  )
+};
 
 export default App;
