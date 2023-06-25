6230a0acf1033f1d73799a26d45806638129a6b1
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-25 15:27:56 -0500

    Changed and App.js content and changed test to make sure text is what is expected

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders correct heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByRole('heading').textContent).toMatch(/our first test/i);
 });
