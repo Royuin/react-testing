f2013a38474e6ad6ba3865d630cba0f2da2fe7f1
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-26 20:37:47 -0500

    Updated test to for rendering changed heading text on user click of button

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,25 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders correct heading', () => {
-  render(<App />);
-  expect(screen.getByRole('heading').textContent).toMatch(/our first test/i);
+describe('App component', () => {
+  test('renders magnificent monkeys', () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('renders radical rhinos after button click', async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+
+    const button = screen.getByRole('button', {name: 'Click Me'});
+
+
+    await user.click(button);
+    expect(screen.getByRole('heading').textContent).toMatch(/radical rhinos/i);
+  });
+
 });
+
